44817650f4fc3351e316b22eedbf0f9d11b6071c
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2025-10-27 17:44:15 +0200

    Add: solution: Stacks & Queues: Implement stack with array

---
+++ Stacks & Queues/exercise-stack-array.js
@@ -0,0 +1,38 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.array = [];
+    }
+
+    peek() {
+        return this.array[this.array.length - 1];
+    }
+
+    push(value) {
+        this.array.push(value);
+        return this;
+    }
+
+    pop() {
+        this.array.pop();
+        return this;
+    }
+
+    isEmpty() {
+        return this.length === 0;
+    }
+}
+
+const myStack = new Stack();
+myStack.push("Google");
+myStack.push("Udemy");
+myStack.push("Coursera");
+console.log(myStack);
+myStack.pop();
+console.log(myStack);
