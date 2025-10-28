f43b7c66e28f7214381aa477ab47a2211a074a2e
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2025-10-28 14:40:11 +0200

    Add: solution: Recursion: fibonacci sequence implementation

---
+++ Recursion/exercise-fibonacci.js
@@ -0,0 +1,26 @@
+// Given a number N return the index value of the Fibonacci sequence, where the sequence is:
+
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ...
+// the pattern of the sequence is that each value is the sum of the 2 previous values, that means that for N=5 → 2+3
+
+//For example: fibonacciRecursive(6) should return 8
+
+
+function fibonacciIterative(n) {
+    const fibonacci = [0, 1];
+    for (let i = 2; i <= n; i++) {
+        fibonacci.push(fibonacci[i - 1] + fibonacci[i - 2]);
+    }
+    return fibonacci[n];
+}
+
+console.log(fibonacciIterative(3));
+
+function fibonacciRecursive(n) {
+    if (n < 2) {
+        return n;
+    }
+    return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+}
+
+console.log(fibonacciRecursive(6));
